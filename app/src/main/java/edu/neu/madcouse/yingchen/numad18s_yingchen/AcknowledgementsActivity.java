d139828310b777f9ff9897506827e4ce92271fbf
Ying Chen <yingchen@YingdeMacBook-Air.local>
2018-06-02 17:09:44 -0700

    add dictionary and acknowledgement activity

---
+++ app/src/main/java/edu/neu/madcouse/yingchen/numad18s_yingchen/AcknowledgementsActivity.java
@@ -0,0 +1,13 @@
+package edu.neu.madcouse.yingchen.numad18s_yingchen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcknowledgementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acknowledgements);
+    }
+}
