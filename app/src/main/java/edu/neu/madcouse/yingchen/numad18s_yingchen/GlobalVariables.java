8da072b633cdd6b242986c9402638c3a6f812ee5
Ying Chen <yingchen@YingdeMacBook-Air.local>
2018-06-24 11:07:28 -0700

    adding code for assignment3

---
+++ app/src/main/java/edu/neu/madcouse/yingchen/numad18s_yingchen/GlobalVariables.java
@@ -0,0 +1,4 @@
+package edu.neu.madcouse.yingchen.numad18s_yingchen;
+
+public class GlobalVariables {
+}
---
+++ app/src/main/java/edu/neu/madcouse/yingchen/numad18s_yingchen/WordGameFragment.java
@@ -0,0 +1,29 @@
+package edu.neu.madcouse.yingchen.numad18s_yingchen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WordGameFragment extends Fragment {
+
+
+    public WordGameFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_word_game, container, false);
+    }
+
+}
